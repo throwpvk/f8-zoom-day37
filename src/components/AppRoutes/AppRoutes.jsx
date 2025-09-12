1768afc738c72d7f011c93767d70d3efc1421907
Phan Van Khai <phanvankhaiofficial@gmail.com>
2025-09-12 09:55:11 +0900

    add scroll to top

--- src/components/AppRoutes/AppRoutes.jsx
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -1,9 +1,11 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
+import ScrollToTop from "../../components/ScrollToTop";
 
 function AppRoutes() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
